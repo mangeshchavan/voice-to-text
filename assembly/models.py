9180c71b8a1b9825aca142909afbe8f1e8f01d6d
mangeshchavan <mangesh.chavan@tejgyan.org>
2025-02-28 13:42:57 +0530

    initial commit

---
+++ assembly/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+import os
+
+class UploadedFile(models.Model):
+    file = models.FileField(upload_to='uploads/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(blank=True, null=True)
+
+    def delete(self, *args, **kwargs):
+        if os.path.isfile(self.file.path):
+            os.remove(self.file.path)
+        super().delete(*args, **kwargs)
---
+++ assembly/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('upload/', views.upload_file, name='upload_file'),
+    path('delete/<int:pk>/', views.delete_file, name='delete_file'),
+    path('convert/<int:pk>/', views.convert_audio_to_text, name='convert'),
+     path('download/<int:pk>/', views.download_text_file, name='download_text'),
+    path('', views.file_list, name='file_list'),
+]
